0d67d03dbdd61f6aadc07023383707c48c058537
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2024-04-15 11:28:59 +0530

    Create user-detail-service-api

---
+++ src/main/java/com/techheaven/userdetailserviceapi/UserDetailServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.techheaven.userdetailserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDetailServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDetailServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techheaven/userdetailserviceapi/UserDetailServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techheaven.userdetailserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDetailServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
