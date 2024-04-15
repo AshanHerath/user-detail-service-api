ce45e9a93367937bfb8262c25a571bf099454390
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2024-04-15 22:04:38 +0530

    implement UserServiceImpl

---
+++ src/main/java/com/techheaven/userdetailserviceapi/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.techheaven.userdetailserviceapi.service.impl;
+
+import com.techheaven.userdetailserviceapi.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public void initializeAdmin() {
+
+    }
+}
